87aae91f97738d9af97fbfd44d449688a2148c08
caoyacheng <caoyacheng5@gmail.com>
2025-03-05 15:29:46 +0800

    comiit

--- vanna_app.py
+++
@@ -1,5 +0,0 @@
-import vanna
-from vanna.remote import VannaDefault
-vn = VannaDefault(model='chinook', api_key=vanna.get_api_key('caoyacheng5@gmail.com'))
-vn.connect_to_sqlite('https://vanna.ai/Chinook.sqlite')
-vn.ask("What are the top 10 albums by sales?")
